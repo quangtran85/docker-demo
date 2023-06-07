14862b431e7fdcf8f2c36aef5f5644b38fc082c7
Quang Tran <quang.tranminh@grevo.net>
2023-06-07 13:37:34 +0700

    fix: Change port and adjust Dockerfile

--- service03/index.js
+++ service03/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3102
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Service 03!</h1>')
+  res.send('<h1>Service 03!</h1><pre>' + JSON.stringify(process.env, null, 4))
 })
 
 app.listen(port, () => {

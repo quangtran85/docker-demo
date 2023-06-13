ef406b5f84d7b3326ba2b1862bdce8aa273f3517
Quang Tran <quang.tranminh@grevo.net>
2023-06-13 21:15:56 +0700

    feat: Update the master file of Cloudformation

--- service02/index.js
+++ service02/index.js
@@ -3,13 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/service02', async (req, res) => {
-  const srv1Res = await fetch(`http://${process.env.SRV1_HOST}/service01`);
-  const content = await srv1Res.text();
-
   res.send(`
     <h1>Service 02!</h1><pre>${JSON.stringify(process.env, null, 4)}</pre>
-    <br><br>
-    <h4>Service01 Response</h4><pre>${content}</pre>
   `)
 })
 
